69a89093ea056e0aedb4edfe92f97b0ce2cbd348
ReYNA0826 <contractselio@gmail.com>
2025-12-08 12:04:02 -0500

    Add paralegales data to paralegales.js

---
+++ src/data/paralegales.js
@@ -0,0 +1,17 @@
+export const paralegales = [
+  {
+    id: 1,
+    nombre: "Jose Zavarce",
+    telefono: "17866757294",
+    email: "zavarceservices@gmail.com",
+    status: "available"
+  },
+  {
+    id: 2,
+    nombre: "Maria Hernandez",
+    telefono: "14074922228",
+    email: "multiservisrh@gmail.com",
+    status: "busy"
+  },
+  ...
+];
