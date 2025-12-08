02e82dc0ba1697d968b69b42a86252a92d8be283
ReYNA0826 <contractselio@gmail.com>
2025-12-08 12:27:47 -0500

    Add paralegales data to paralegales.js

---
+++ data/paralegales.js
@@ -0,0 +1,18 @@
+export const paralegales = [
+  { id: 1, nombre: "Jose Zavarce", telefono: "17866757294", email: "zavarceservices@gmail.com", status: "available" },
+  { id: 2, nombre: "Maria Hernandez", telefono: "14074922228", email: "multiservisrh@gmail.com", status: "available" },
+  { id: 3, nombre: "Fabricio González", telefono: "17863404964", email: "fabriciogm@hotmail.com", status: "available" },
+  { id: 4, nombre: "Oscar Gutierrez", telefono: "19546278616", email: "gmunited7@gmail.com", status: "busy" },
+  { id: 5, nombre: "Walmelys Flores", telefono: "17862707931", email: "walmelysfloresparalegal@gmail.com", status: "available" },
+  { id: 6, nombre: "Karina Tirado", telefono: "19546278616", email: "ktiradoinmigracion@gmail.com", status: "busy" },
+  { id: 7, nombre: "Leidys Godoy", telefono: "14076683238", email: "leidysgodoy@hotmail.com", status: "available" },
+  { id: 8, nombre: "Adela Martinez", telefono: "13056398031", email: "inmigrantequeconquista@gmail.com", status: "available" },
+  { id: 9, nombre: "Nelisa Pena", telefono: "17866837074", email: "nelisap@eliolawteam.com", status: "available" },
+  { id: 10, nombre: "DANIEL ARIAS", telefono: "14074080732", email: "dariasimmigration@gmail.com", status: "available" },
+  { id: 11, nombre: "Tony Marval", telefono: "17864707328", email: "marvalabogado@hotmail.com", status: "busy" },
+  { id: 12, nombre: "Cioly Zambrano", telefono: "17737327003", email: "serviciosmigratorios3@gmail.com", status: "available" },
+  { id: 13, nombre: "Axa Rebeca Perez Petit", telefono: "17866093929", email: "axagonzalez5@gmail.com", status: "available" },
+  { id: 14, nombre: "Maria de Gaetano", telefono: "14077472095", email: "mdgs2323@gmail.com", status: "available" },
+  { id: 15, nombre: "Leonardo Perez", telefono: "17542171022", email: "leonardopsemeco@gmail.com", status: "available" },
+  { id: 16, nombre: "Minerva Mendoza Paipa", telefono: "19548716641", email: "minervap@eliolawteam.com", status: "available" }
+];
